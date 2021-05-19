6d15d7f4704894a976368ba0336440aa38c00487
Alfonso Buononato <74600557+AlfonsoBuononato@users.noreply.github.com>
2021-05-19 17:52:22 +0200

    esercizio

--- index.php
+++ index.php
@@ -56,6 +56,8 @@ $faq = [
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/style.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;700&display=swap" rel="stylesheet"> 
     <title>Document</title>
 </head>
 <body>
